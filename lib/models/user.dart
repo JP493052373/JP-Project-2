3ae5f08cb38902e7e5e80b6b9e515d4186a7deec
JP <fantasma11747@gmail.com>
2023-12-01 11:31:02 -0300

    Project 2 JP

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String id;
+  final String nome;
+  final String email;
+  final String avatarURL;
+
+  User({
+    required this.id,
+    required this.nome,
+    required this.email,
+    this.avatarURL = '',
+  });
+}
---
+++ lib/providers/users.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/data/dummy_users.dart';
+import 'package:flutter_crud/models/user.dart';
+
+class Users with ChangeNotifier {
+  Map<String, User> _items = {...DUMMY_USERS};
+
+  List<User> get all {
+    return [..._items.values];
+  }
+
+  int get count {
+    return _items.length;
+  }
+
+  User byIndex(int indice) {
+    return _items.values.elementAt(indice);
+  }
+
+  void put(User user) {
+    if (user.id.trim().isNotEmpty && _items.containsKey(user.id)) {
+      //update:
+      _items.update(
+        user.id,
+        (_) => User(
+          id: user.id,
+          nome: user.nome,
+          email: user.email,
+          avatarURL: user.avatarURL,
+        ),
+      );
+    } else {
+      final id = Random().nextDouble().toString();
+
+      _items.putIfAbsent(
+        id,
+        () => User(
+          id: id,
+          nome: user.nome,
+          email: user.email,
+          avatarURL: user.avatarURL,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+}
